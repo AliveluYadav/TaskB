d4343677ffbbc0ab90d190a5a8842c6d0700700e
Alivelu <alivelu7193@gmail.com>
2019-07-03 16:03:41 +0530

    first commit

---
+++ CollectionVC.swift
@@ -0,0 +1,71 @@
+//
+//  CollectionVC.swift
+//  TaskB
+//
+//  Created by AktrixIOS01 on 18/06/18.
+//  Copyright © 2018 Aktrix. All rights reserved.
+//
+
+import UIKit
+
+class CollectionVC: UIViewController,UICollectionViewDataSource,UICollectionViewDelegate {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var displyImgArry=[Images]()
+    
+    var arr = [UIImage]()
+    
+    var array11 = ["shiva","sai","Mahi","rajaa"]
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        do{
+            
+            dataSaved=try conte.fetch(Images.fetchRequest())
+
+            for everyUser in dataSaved
+            {
+                let image : UIImage = UIImage(data: everyUser.img!)!
+                
+                arr.append(image)
+                
+                print(arr)
+            }
+            
+            DispatchQueue.main.async {
+                
+                self.collectionView.reloadData()
+            }
+        }
+            
+        catch{
+
+        }
+    }
+
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+     return arr.count
+   
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
+    {
+        
+        let cell=collectionView.dequeueReusableCell(withReuseIdentifier: "cell123", for: indexPath) as! CollectionViewCell
+    
+        
+      cell.imgInCV.image =  arr[indexPath.row]
+        
+   //  cell.lbl.text = array11[indexPath.row]
+    
+        
+        return cell
+    }
+
+}
